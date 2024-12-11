d11cb7d849aad4237dec7edbb739726ebba61d17
Víctor J <maximovj@outlook.com>
2024-12-11 03:19:10 -0600

    * Iniciando repositorio...

---
+++ linuxhub-api/src/main/java/com/github/maximovj/linuxhubapi/linuxhub_api/LinuxhubApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.maximovj.linuxhubapi.linuxhub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxhubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxhubApiApplication.class, args);
+	}
+
+}
---
+++ linuxhub-api/src/test/java/com/github/maximovj/linuxhubapi/linuxhub_api/LinuxhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maximovj.linuxhubapi.linuxhub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxhubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
