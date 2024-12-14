f2d5db71904a943f101edb94d0b1e944ac339e7c
Víctor J <maximovj@outlook.com>
2024-12-14 12:05:12 -0600

    * dev-springbot-v3 : Se agrega nuevo API para distribuciones

---
+++ linuxhub-distributions-api/src/main/java/com/github/maximovj/linuxhub/distributions/api/linuxhub_distributions_api/LinuxhubDistributionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.maximovj.linuxhub.distributions.api.linuxhub_distributions_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxhubDistributionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxhubDistributionsApiApplication.class, args);
+	}
+
+}
---
+++ linuxhub-distributions-api/src/test/java/com/github/maximovj/linuxhub/distributions/api/linuxhub_distributions_api/LinuxhubDistributionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maximovj.linuxhub.distributions.api.linuxhub_distributions_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxhubDistributionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
