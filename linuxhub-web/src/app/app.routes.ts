9a07be15fcc6e4b01a53e31356b31c71e8664e3e
Víctor J <maximovj@outlook.com>
2024-12-21 09:35:01 -0600

    * dev-angular-v19 : Se agrega página de inicio

--- linuxhub-web/src/app/app.routes.ts
+++ linuxhub-web/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import("./distribution/page/home/home-page.component").then( c => c.HomePageComponent),
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '',
+  }
+];
---
+++ linuxhub-web/src/app/distribution/page/home/home-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  styles: ``,
+  template: `
+    <h1>Hola mundo</h1>
+  `,
+})
+export class HomePageComponent
+{
+
+  title = "Home Page";
+
+}
