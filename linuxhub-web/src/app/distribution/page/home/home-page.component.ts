a862b551846b9b677f2c16dbe94ed2b8d02642df
Víctor J <maximovj@outlook.com>
2024-12-25 16:17:00 -0600

    * dev-angular-v19 : Se agrega distribuciones de linux
    !!! feature
    - Se agrega distribuciones de linux

--- linuxhub-web/src/app/distribution/page/home/home-page.component.ts
+++ linuxhub-web/src/app/distribution/page/home/home-page.component.ts
@@ -1,18 +1,75 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { NavBarComponent } from "../../../shared/components/navbar/navbar-component";
+import { DistributionsService } from "../../services/distributions-service";
+import { catchError, delay, map, Observable, of, startWith } from "rxjs";
+import { Distribution } from "../../interfaces";
+import { CommonModule } from "@angular/common";
 
 @Component({
   imports: [
     NavBarComponent,
+    CommonModule,
   ],
   styles: ``,
   template: `
     <shared-navbar-component></shared-navbar-component>
+    <div class="container mx-auto">
+    <ng-container *ngIf="distributions$ | async as res">
+      <div *ngIf="res.loading; else content">
+        <p class="text-center font-bold text-2xl">Cargando información...</p>
+      </div>
+      <ng-template #content>
+        <div *ngIf="res.err; else distributions;">
+          {{ res.err }}
+        </div>
+        <ng-template #distributions>
+          @if (res.data.length <= 0) {
+            <p class="text-center font-bold text-base">Sin distribuciones que mostrar <mark>:'(</mark></p>
+          } @else {
+            <ng-container>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+              @for (item of res.data; track $index) {
+                <div class="flex flex-row justify-start gap-4 bg-slate-600 text-white border-2 border-red-900 rounded shadow-lg shadow-slate-700">
+                  <div class="bg-slate-800">
+                    <img class="size-32 rounded-md object-scale-down hover:scale-75" :src="{{ item.logo }}" :alt="{{ item.logo }}" :srcset="{{ item.logo }}" />
+                  </div>
+                  <div class="flex flex-col align-middle justify-start gap-4">
+                    <a class="font-black shadow-rose-700 shadow-lg text-end pr-4 rounded-md mt-2" :href="{{ item.official_site }}" target="_blank">{{ item.name }}</a>
+                    <p class="w-full max-w-60 text-wrap whitespace-break-spaces">{{ item.description }}</p>
+                  </div>
+                </div>
+              }
+            </div>
+            </ng-container>
+          }
+        </ng-template>
+      </ng-template>
+    </ng-container>
+    </div>
   `,
 })
-export class HomePageComponent
+export class HomePageComponent implements OnInit
 {
-
   title = "Home Page";
+
+  distributionService = inject(DistributionsService);
+
+  public distributions$ !:Observable<any>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.distributions$ = this.distributionService
+      .listDistributions()
+      .pipe(
+        map( (data: Distribution[] | []) => {
+          console.log("async pipe: ", data);
+          return {loading: false, data, err: null};
+        }),
+        startWith({loading: true, data: null, err: null}),
+        catchError( (err) => of({ loading: false, data: null, err }) )
+      );
+  }
+
 
 }
--- linuxhub-web/src/app/distribution/services/distributions-service.ts
+++ linuxhub-web/src/app/distribution/services/distributions-service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { environments } from "../../../environments/environments";
+import { HttpClient } from "@angular/common/http";
+import { catchError, map, Observable, throwError } from "rxjs";
+import { DistributionsResponse } from "../interfaces/distributions-response-interface";
+import { Distribution } from "../interfaces";
 
 @Injectable({
   providedIn: "root"
 })
 export class DistributionsService
 {
-  ANGULAR_BASEURL_LINUX_DISTRIBUTIONS_API :string = environments.ANGULAR_BASEURL_LINUX_DISTRIBUTIONS_API;
+  private ANGULAR_BASEURL_LINUX_DISTRIBUTIONS_API :string = environments.ANGULAR_BASEURL_LINUX_DISTRIBUTIONS_API;
+
+  private http = inject(HttpClient);
+
+  constructor() { }
+
+  public listDistributions() : Observable<Distribution[]>
+  {
+    return this.http.get<DistributionsResponse>(`${this.ANGULAR_BASEURL_LINUX_DISTRIBUTIONS_API}/v1/distribution`)
+    .pipe(
+      map(({success, data}) => {
+        if(success && data?.items) {
+          //console.log(`'DistributionsService::map | start'`);
+          //console.log(`'map: '`, {data});
+          //console.log(`'map: '`, {success});
+          return data?.items;
+        }
+        return [];
+      }),
+      catchError( (err) => throwError(() => err.message)),
+    );
+  }
 
 }
