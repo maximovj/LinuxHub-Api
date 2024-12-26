cc8bbb3a6647a70abcc060805f1e6ceb15e213ac
Víctor J <maximovj@outlook.com>
2024-12-25 18:13:42 -0600

    * dev-angular-v19 : Se agrega distribuciones de linux
    !!! feature
    - Se agrega distribuciones de linux

--- linuxhub-web/src/app/distribution/page/home/home-page.component.ts
+++ linuxhub-web/src/app/distribution/page/home/home-page.component.ts
@@ -27,15 +27,21 @@ import { CommonModule } from "@angular/common";
             <p class="text-center font-bold text-base">Sin distribuciones que mostrar <mark>:'(</mark></p>
           } @else {
             <ng-container>
-            <div class="container m-auto grid grid-cols-1 md:grid-cols-3 gap-10">
+            <div class="container m-auto grid grid-cols-1 lg:grid-cols-3 gap-10">
               @for (item of res.data; track $index) {
-                <div class="flex flex-row justify-start gap-4 bg-slate-600 text-white border-2 border-red-900 rounded shadow-lg shadow-slate-700">
-                  <div class="bg-slate-800">
+                <div class="flex flex-col justify-start gap-4 bg-slate-600 text-white border-2 border-red-900 rounded shadow-lg shadow-slate-700 lg:flex-row">
+                  <div class="bg-slate-800 justify-items-center">
                     <img class="size-32 rounded-md object-scale-down hover:scale-75" :src="{{ item.logo }}" :alt="{{ item.logo }}" :srcset="{{ item.logo }}" />
                   </div>
                   <div class="flex flex-col align-middle justify-start gap-4">
-                    <a class="font-black shadow-rose-700 shadow-lg text-end pr-4 rounded-md mt-2" :href="{{ item.official_site }}" target="_blank">{{ item.name }}</a>
-                    <p class="w-full max-w-60 text-wrap whitespace-break-spaces">{{ item.description }}</p>
+                    <a class="font-black shadow-rose-700 shadow-lg text-end pr-4 rounded-md mt-2 text-white hover:text-yellow-400" :href="{{ item.official_site }}" target="_blank">{{ item.name }}</a>
+                    <p class="w-full max-w-60 text-wrap whitespace-break-spaces hidden lg:block">{{ item.description }}</p>
+                    <ul class="p-4">
+                      <li>Detalles técnicos</li>
+                      <li *ngFor="let tec of item.technician | keyvalue">
+                        <span class="text-slate-300 text-base">{{ tec.key }}</span> : <span>{{ tec.value }}</span>
+                      </li>
+                    </ul>
                   </div>
                 </div>
               }
